f36092a6577821366e65f395ccacd0641afc8e2e
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2026-03-26 22:17:26 +0200

    creating role model

---
+++ src/main/java/com/example/demo/model/RoleModel.java
@@ -0,0 +1,42 @@
+package com.example.demo.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Document(collection="roles")
+@Setter
+@Getter
+@ToString
+public class RoleModel {
+
+    @Id
+    public String id;
+
+    public String name;
+    public String description;
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public String setName(String Name){
+        return this.name= name;
+    }
+
+    public String setDescription(String description){
+        return this.description = description;
+    }
+
+    
+    
+}
