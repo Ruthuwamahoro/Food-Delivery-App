8279a6a47c835fe098b0452826c34750c6ca5cd3
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2026-04-12 20:52:01 +0200

    adding token to http header

--- src/main/java/com/example/demo/controller/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/example/demo/controller/OAuth2LoginSuccessHandler.java
@@ -78,7 +78,7 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler{
                                 .maxAge(Duration.ofDays(7))
                                 .build();
         response.addHeader(HttpHeaders.SET_COOKIE,cookie.toString());
-        response.sendRedirect(frontendUrl + "/");
+        response.sendRedirect(frontendUrl + "/?token="+ token);
     
         // response.setContentType("application/json");
         // response.setStatus(HttpServletResponse.SC_OK);
